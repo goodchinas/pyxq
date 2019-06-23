979a863b854818e6ddd2d161d18ab52b9ccd7067
tim <goodchinas@>
2019-06-23 13:30:43 +0800

    update config

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
     ],
-    description="python quantis so easy.",
+    description="python quant is so easy.",
     install_requires=requirements,
     license="MIT license",
     long_description=readme + '\n\n' + history,
