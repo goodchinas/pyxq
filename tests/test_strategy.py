5bcb3c2c8c3dbf99db7f8e62214d438bd4d6a0ec
tim <goodchinas@qq.com>
2020-01-17 18:42:17 +0800

    1. delete the test data file and test;

--- tests/test_strategy.py
+++ tests/test_strategy.py
@@ -2,7 +2,6 @@ import itertools
 import typing as tp
 import unittest
 from datetime import date, datetime
-from os import path
 
 import numpy as np
 import pandas as pd
@@ -74,47 +73,11 @@ def test0():
     exchange = actor.Exchange(broker=cb.CallBackManager())
     strategy = Strategy(acc=acc, broker=cb.CallBackManager())
     broker = actor.Broker(acc=acc, gateway=strategy.broker, exchange=exchange.broker)
-    # 配置
-    cash, symbol = 100000, '000002'
-    broker.on_cash(pa.Cash(num=cash, dt=datetime.now()))
-    broker.on_contract(pa.ContractMsg(
-        symbol=symbol, dt=datetime.now(), cm=pa.ContractMod(num_per_unit=100, value_per_dot=1, margin_ratio=1)))
-    broker.on_commission(
-        pa.CommissionMsg(
-            dt=datetime.now(),
-            symbol=symbol,
-            cm=pa.CommissionStockA(tax=0.001, commission=0.00025, min_commission=5),
-        ))
-    # 读取数据
-    symbol = '000002'
-    file = path.join(path.dirname(path.realpath(__file__)), rf'data/{symbol}.csv')
-    print(file)
-    # data = pd.read_csv(path.abspath(f'data/{symbol}.csv'))
-    # data = pd.read_csv(f'data/{symbol}.csv')
-    data = pd.read_csv(file)
-    # 行情事件
-    for i, d in data.iloc[:, :].iterrows():
-        _dt = datetime.strptime(d.timestamp, '%Y-%m-%d')
-        exchange.on_open(md.Open(dt=_dt))
-        exchange.on_tick(md.Tick(dt=_dt, symbol=symbol, price=d.open, volume=0))
-        _a = acc
-        # print("权益:{:>10.2f} 可用:{:>10.2f} 保证:{:>10.2f} 冻结:{:>10.2f} 收益:{:>10.2f} 手续费:{:>5.2f} "
-        #       "开:{:>6.2f} 高:{:>6.2f} 低:{:>6.2f} 收:{:>6.2f}".format(
-        #     _a.equity, _a.free, _a.margin, _a.frozen, _a.profit, _a.commission, d.open, d.high, d.low, d.close))
-        exchange.on_close(md.Close(dt=_dt))
-
-
-def test1():
-    # 构建框架
-    acc = account.Account()
-    exchange = actor.Exchange(broker=cb.CallBackManager())
-    strategy = Strategy(acc=acc, broker=cb.CallBackManager())
-    broker = actor.Broker(acc=acc, gateway=strategy.broker, exchange=exchange.broker)
     # 参数项目
     start_date = datetime(year=2018, month=1, day=1)
     # 模拟行情
     np.random.seed(8)
-    _n = 4000 * 24
+    _n = 2000 * 24
     _s = [chr(_) for _ in range(ord('A'), ord("A") + 1)]
     _data = np.random.normal(loc=.0, scale=0.01, size=(_n, len(_s)))
     _data = np.exp(_data.cumsum(axis=0))
@@ -163,10 +126,6 @@ def test1():
 class TestService(unittest.TestCase):
     def test_test0(self):
         test0()
-        self.assertTrue(True)
-
-    def test_test1(self):
-        test1()
         self.assertTrue(True)
 
     def test_ma_signal(self):
