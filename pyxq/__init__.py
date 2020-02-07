c5555cf9b1bc599827153cc1fe8771a785ab4c80
tim <goodchinas@qq.com>
2020-02-07 18:57:43 +0800

    1. CI

--- pyxq/__init__.py
+++ pyxq/__init__.py
@@ -6,4 +6,4 @@ __author__ = """tim"""
 __email__ = 'goodchinas@qq.com'
 __version__ = '0.1.5.r0'
 
-from . import actor, app, msg, service
+from . import actor, app, msg, service, ba, cb, cn, itf
--- setup.py
+++ setup.py
@@ -2,18 +2,26 @@
 # -*- coding: utf-8 -*-
 
 """The setup script."""
+import os
 
 from setuptools import setup
 
 with open('README.MD') as readme_file:
     readme = readme_file.read()
 
-# with open('HISTORY.rst') as history_file:
-#     history = history_file.read()
+
+def get_req(file: str) -> list:
+    _r = []
+    with open(os.path.join(
+        os.path.abspath(os.path.dirname(__file__)),
+        file
+    )) as f:
+        _r.extend([l for l in f.read().splitlines() if not l.startswith('#')])
+    return _r
 
 
 setup_requirements = []
 
 setup(
-    install_requires=[],
+    install_requires=get_req('requirements.txt'),
 )
